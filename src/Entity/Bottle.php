b2e05a1ebe5ae923c867c73914c9a182cb64fb79
gigi13 <gigithebest@live.fr>
2019-11-12 12:31:55 +0100

    modif page menu navbar

---
+++ src/Entity/Bottle.php
@@ -0,0 +1,210 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\BottleRepository")
+ */
+class Bottle
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $degree;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $vintage;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $quantity;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $designation;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $consumption_date;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="boolean", options={"default": false})
+     */
+    private $sold;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $picture;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+{
+    $this->name = $name;
+
+    return $this;
+}
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDegree(): ?int
+    {
+        return $this->degree;
+    }
+
+    public function setDegree(int $degree): self
+{
+    $this->degree = $degree;
+
+    return $this;
+}
+
+    public function getVintage(): ?int
+    {
+        return $this->vintage;
+    }
+
+    public function setVintage(int $vintage): self
+{
+    $this->vintage = $vintage;
+
+    return $this;
+}
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): self
+{
+    $this->quantity = $quantity;
+
+    return $this;
+}
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+{
+    $this->price = $price;
+
+    return $this;
+}
+
+    public function getDesignation(): ?string
+    {
+        return $this->designation;
+    }
+
+    public function setDesignation(string $designation): self
+{
+    $this->designation = $designation;
+
+    return $this;
+}
+
+    public function getConsumptionDate(): ?\DateTimeInterface
+    {
+        return $this->consumption_date;
+    }
+
+    public function setConsumptionDate(\DateTimeInterface $consumption_date): self
+{
+    $this->consumption_date = $consumption_date;
+
+    return $this;
+}
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+{
+    $this->type = $type;
+
+    return $this;
+}
+
+    public function getSold(): ?bool
+    {
+        return $this->sold;
+    }
+
+    public function setSold(bool $sold): self
+{
+    $this->sold = $sold;
+
+    return $this;
+}
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(string $picture): self
+{
+    $this->picture = $picture;
+
+    return $this;
+}
+}
